cd329b87ff282815722c1a50396d691c983a92c6
anisbsalah <anisbensalah.89@gmail.com>
2023-10-29 17:11:39 +0100

    v2023-10-27

---
+++ README.md
@@ -0,0 +1,32 @@
+# Dracula theme
+
+## How to install
+
+* Install `git` first:
+
+```bash
+### On debian based distros:
+sudo apt install git
+
+### On ArchLinux:
+sudo pacman -S git
+```
+
+* Clone the repository:
+
+```bash
+git clone https://github.com/anisbsalah/Dracula-Theme.git /tmp/Dracula-Theme
+```
+
+* Run `dracula.sh`:
+
+```bash
+cd /tmp/Dracula-Theme
+./dracula.sh
+```
+
+* Select the theme of your choice with the system settings manager.
+
+## Credits
+
+* [dracula/dracula-theme](https://github.com/dracula/dracula-theme)
---
+++ dracula.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo cp -rf Dracula/aurorae/* /usr/share/aurorae/themes/
+sudo cp -rf Dracula/color-schemes/* /usr/share/color-schemes/
+sudo cp -rf Dracula/cursors/* /usr/share/icons/
+sudo cp -rf Dracula/desktoptheme/* /usr/share/plasma/desktoptheme/
+sudo cp -rf Dracula/gtk/* /usr/share/themes/
+sudo cp -rf Dracula/konsole/* /usr/share/konsole/
+sudo cp -rf Dracula/Kvantum/* /usr/share/Kvantum/
+sudo cp -rf Dracula/look-and-feel/* /usr/share/plasma/look-and-feel/
+sudo cp -rf Dracula/plymouth/* /usr/share/plymouth/themes/
+sudo cp -rf Dracula/sddm/* /usr/share/sddm/themes/
+sudo cp -rf Dracula/wallpapers/* /usr/share/backgrounds/
